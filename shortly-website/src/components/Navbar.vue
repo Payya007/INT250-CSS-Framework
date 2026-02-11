2390e55341e95e13573dfb4e51de39d9f47be5bc
Student <Student@Train11-2-PC348.control.ad.sit.kmutt.ac.th>
2026-02-11 17:09:49 +0700

    .

---
+++ shortly-website/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<template>
+    <nav class="flex item-center justifly-bet">
+        <div>
+            <div>
+                <!-- logo -->
+                <img src="" alt="">
+                <!-- left menu -->
+                 <div>
+                    <a href="#">Fetures</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Resources</a>
+                 </div>
+
+                 <!-- right Button Mn -->
+                  <div>
+                    <a href="#">Login</a>
+                    <a href="#">Sign up</a>
+                  </div>
+                  <!-- Hamburger Button -->
+            </div>
+        </div>
+
+    </nav>
+</template>
