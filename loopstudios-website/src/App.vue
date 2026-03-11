239d97a0c83f292261f99bf25f1ca3b86ada0517
Student <Student@Train11-2-PC364.control.ad.sit.kmutt.ac.th>
2026-03-11 16:25:40 +0700

    add work week 8

---
+++ loopstudios-website/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import {ref} from "vue";
+
+const menuItems = ref([
+  { name: "About", link: "#" },
+  { name: "Careers", link: "#" },
+  { name: "Events", link: "#" },
+  { name: "Products", link: "#" },
+  { name: "Supports", link: "#" },
+])
+</script>
+
+<template>
+  
+  <Navbar :menu-items="menuItems"/>
+</template>
+
+<style scoped></style>
---
+++ loopstudios-website/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  menuItems: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+
+<template>
+    <!-- Hero Section -->
+    <section
+        class="bg-[url(assets/mobile/image-hero.jpg)] bg-no-repeat bg-cover md:bg-[url(assets/desktop/image-hero.jpg)] md:bg-no-repeat md:bg-cover">
+
+        <div class="container max-w-6xl mx-auto px-6 py-12">
+            <nav class="flex items-center justify-between font-bold text-white">
+                <img src="../assets/logo.svg" alt="Loopstudios Logo">
+                <!-- Menu -->
+                <div class="hidden h-10 font-sans md:flex md:space-x-8">
+                    <div v-for="item in menuItems" :key="item.name" class="group">
+                       <a :href="item.link">{{ item.name }}</a>
+                        <div class="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
